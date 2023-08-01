5b0b692ee69c599ca66f006f0a9a59b8ab78056f
Vanja Oljaca <vanja.oljaca@me.com>
2023-08-01 00:15:49 -1000

    idk

---
+++ wallaby.js
@@ -0,0 +1,18 @@
+module.exports = function (wallaby) {
+    return {
+        files: [
+            'tsconfig.json',
+            'src/**/*.ts?(x)',
+            '!src/**/*.test.ts?(x)',
+        ],
+
+        tests: ['src/**/*.test.ts?(x)'],
+
+        env: {
+            type: 'node',
+            runner: 'node',
+        },
+
+        testFramework: 'jest',
+    };
+};
